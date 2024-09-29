fc330bbf4b2e7fec5f2433018bef5e1f40913544
Carlos de Almeida <carlos_a_brito@hotmail.com>
2024-09-28 23:46:06 -0300

    Melhoria na classe UserNotFoundExceptionMapper

--- src/main/java/almeidabrito/exception/UserNotFoundExceptionMapper.java
+++ src/main/java/almeidabrito/exception/UserNotFoundExceptionMapper.java
@@ -8,8 +8,6 @@ import jakarta.ws.rs.ext.Provider;
 public class UserNotFoundExceptionMapper implements ExceptionMapper<UserNotFoundException> {
     @Override
     public Response toResponse(UserNotFoundException exception) {
-        //return Response.status(Response.Status.NOT_FOUND.getStatusCode(), reasonPhrase: "User not found!").build();
-        //return Response.status(Response.Status.NOT_FOUND.getStatusCode()).build();
         return Response.status(Response.Status.NOT_FOUND)
                 .entity("User not found!")
                 .build();
